68d20ec5a814a08fdfe44db6b659d7eff11bfa0f
Filipe Gianino <filipegianinom@gmail.com>
2023-10-06 11:02:39 -0300

    last update

---
+++ tic-tac-toe/src/App.js
@@ -0,0 +1,13 @@
+
+import './App.css';
+import TicTacToe from './components/TicTacToe/tictactoe';
+
+function App() {
+  return (
+    <div>
+      <TicTacToe/>
+    </div>
+  );
+}
+
+export default App;
